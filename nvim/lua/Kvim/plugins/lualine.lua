74c95a949ea276d4c1c4a3a756f54f50282dabaf
Kepler <kyplerr@gmail.com>
2024-05-13 19:38:01 -0700

    Changed Lualine plugin theme

--- nvim/lua/Kvim/plugins/lualine.lua
+++ nvim/lua/Kvim/plugins/lualine.lua
@@ -1,6 +1,7 @@
 require("lualine").setup {
     options = {
       icons_enabled = true,
+      theme = "cyberdream";
       component_separators = { left = "", right = ""},
       section_separators = { left = "", right = "" },
       disabled_filetypes = {
