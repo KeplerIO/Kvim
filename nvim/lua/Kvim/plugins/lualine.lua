e8f9f26af63bfd58625a2b5ab572f9b8792dc36b
Kepler <kyplerr@gmail.com>
2024-05-13 20:26:17 -0700

    Increase padding

--- nvim/lua/Kvim/plugins/lualine.lua
+++ nvim/lua/Kvim/plugins/lualine.lua
@@ -1,6 +1,14 @@
 require("lualine").setup({
-  options = {
-    icons_enabled = true,
-    theme = "OceanicNext",
-  }
+	options = {
+		icons_enabled = true,
+		theme = "OceanicNext",
+	},
+	sections = {
+		lualine_a = {
+			{
+				"mode",
+				padding = 3
+			}
+		}
+	}
 });
